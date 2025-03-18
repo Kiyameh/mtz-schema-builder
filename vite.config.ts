343decbf6d2c5af023773dab9b0b5ea726d109ee
Kiyameh <kiyameh@outlook.com>
2025-03-18 19:43:47 +0100

    Corregio path

--- vite.config.ts
+++ vite.config.ts
@@ -10,5 +10,5 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  base: '/mtz-schema-builder/',
+  base: process.env.VITE_BASE_PATH || '/mtz-schema-builder',
 })
